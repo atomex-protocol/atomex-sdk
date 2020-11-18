064d59ab1e77094694fc16d74be33c208abe4089
Soumya Ghosh Dastidar <gdsoumya@gmail.com>
2020-11-18 09:17:29 +0530

    Added Tests for the new helper functions
    Signed-off-by: Soumya Ghosh Dastidar <gdsoumya@gmail.com>

--- test/atomex.spec.ts
+++ test/atomex.spec.ts
@@ -1,9 +1,11 @@
 import fetch from "node-fetch";
 import { mocked } from "ts-jest/utils";
+import { OrderPreview } from "../lib/types";
+import { Atomex } from "../src/atomex";
+import { AuthTokenRequest, AuthTokenResponse, OrderBook } from "../src/types";
+import orderBook from "./data/order_book.json";
 import spec from "./data/swagger.json";
 const OpenAPISampler = require("openapi-sampler");
-import { Atomex } from "../src/atomex";
-import { AuthTokenRequest, AuthTokenResponse } from "../src/types";
 
 jest.mock("node-fetch", () => {
   return jest.fn();
@@ -19,7 +21,8 @@ describe("Atomex test", () => {
 
     const req = <AuthTokenRequest>(
       OpenAPISampler.sample(
-        spec.paths["/v1/Token"].post.requestBody.content["application/json"].schema,
+        spec.paths["/v1/Token"].post.requestBody.content["application/json"]
+          .schema,
         {},
         spec,
       )
@@ -31,7 +34,8 @@ describe("Atomex test", () => {
     expect(typeof req.algorithm).toBe("string");
 
     const res = OpenAPISampler.sample(
-      spec.paths["/v1/Token"].post.responses[200].content["application/json"].schema,
+      spec.paths["/v1/Token"].post.responses[200].content["application/json"]
+        .schema,
       {},
       spec,
     );
@@ -50,7 +54,9 @@ describe("Atomex test", () => {
     expect(mocked(fetch).mock.calls.length).toBe(1);
     expect(mocked(fetch).mock.calls[0][0]).toBe("http://localhost/v1/Token");
     expect(mocked(fetch).mock.calls[0][1]?.method).toBe("post");
-    expect(mocked(fetch).mock.calls[0][1]?.headers).toHaveProperty("Content-Type");
+    expect(mocked(fetch).mock.calls[0][1]?.headers).toHaveProperty(
+      "Content-Type",
+    );
     expect(mocked(fetch).mock.calls[0][1]?.body).toBeDefined();
 
     expect(typeof resp.expires).toBe("number");
@@ -62,7 +68,9 @@ describe("Atomex test", () => {
     const atomex = new Atomex("http://localhost", "");
 
     const res = OpenAPISampler.sample(
-      spec.paths["/v1/MarketData/quotes"].get.responses[200].content["application/json"].schema,
+      spec.paths["/v1/MarketData/quotes"].get.responses[200].content[
+        "application/json"
+      ].schema,
       {},
       spec,
     );
@@ -79,13 +87,77 @@ describe("Atomex test", () => {
 
     const resp = await atomex.getQuotes(["XTZ/ETH"]);
     expect(mocked(fetch).mock.calls.length).toBe(1);
-    expect(mocked(fetch).mock.calls[0][0]).toBe("http://localhost/v1/MarketData/quotes?symbols=XTZ%2FETH");
+    expect(mocked(fetch).mock.calls[0][0]).toBe(
+      "http://localhost/v1/MarketData/quotes?symbols=XTZ%2FETH",
+    );
     expect(mocked(fetch).mock.calls[0][1]?.method).toBe("get");
-    
+
     expect(resp.length).toBe(1);
     expect(typeof resp[0].symbol).toBe("string");
     expect(typeof resp[0].timeStamp).toBe("number");
     expect(typeof resp[0].bid).toBe("number");
     expect(typeof resp[0].ask).toBe("number");
+  });
+
+  test("getOrderSize", () => {
+    const atomex = new Atomex("http://localhost");
+    const amount = 1000,
+      price = 0.01;
+    let res = atomex.getOrderSize(1000, 0.01, "Buy", "Send");
+    let expectedRes = {
+      orderSize: amount / price,
+      amountExpected: amount / price,
+    };
+    expect(res).toStrictEqual(expectedRes);
+    res = atomex.getOrderSize(1000, 0.01, "Sell", "Receive");
+    expect(res).toStrictEqual(expectedRes);
+
+    res = atomex.getOrderSize(1000, 0.01, "Buy", "Receive");
+    expectedRes = {
+      orderSize: amount,
+      amountExpected: amount * price,
+    };
+    expect(res).toStrictEqual(expectedRes);
+    res = atomex.getOrderSize(1000, 0.01, "Sell", "Send");
+    expect(res).toStrictEqual(expectedRes);
+  });
+
+  test("getOrderPreview", () => {
+    const atomex = new Atomex("http://localhost");
+
+    try {
+      // should throw error as 1:1 match not found
+      atomex.getOrderPreview(orderBook as OrderBook, "Buy", 1000, "Send");
+      expect(true).toBe(false);
+    } catch (e) {
+      expect(e.message).toBe("No matching entry found");
+    }
+    // finds least matching entry
+    let orderPreview = atomex.getOrderPreview(
+      orderBook as OrderBook,
+      "Buy",
+      1000,
+      "Receive",
+    );
+    let expectedValue: OrderPreview = {
+      amountReceived: 1000,
+      amountSent: 1,
+      price: 0.001,
+    };
+    expect(orderPreview).toStrictEqual(expectedValue);
+
+    // finds max matching entry
+    orderPreview = atomex.getOrderPreview(
+      orderBook as OrderBook,
+      "Sell",
+      1500,
+      "Send",
+    );
+    expectedValue = {
+      amountReceived: 166.5,
+      amountSent: 1500,
+      price: 0.111,
+    };
+    expect(orderPreview).toStrictEqual(expectedValue);
   });
 });
--- test/ethereum.spec.ts
+++ test/ethereum.spec.ts
@@ -1,19 +1,17 @@
 import Web3 from "web3";
 import { Transaction } from "web3-core";
-import { AbiItem } from "web3-utils";
+// @ts-ignore
+import FakeProvider from "web3-fake-provider";
 import config from "../src/config.json";
-import { EthereumHelpers } from "../src/ethereum";
 import { now } from "../src/helpers";
 import initiate_tx from "./data/eth_initiate_tx.json";
+import { GetEthereumHelperInstance } from "./helpers/ethereum";
 
 describe("EthereumHelpers test", () => {
-  const eth = new EthereumHelpers(
-    new Web3(),
-    config.contracts.ETH.abi as AbiItem[],
-    config.contracts.ETH.testnet.address,
-    config.rpc.ethereum.testnet.blockTime,
-    config.contracts.ETH.testnet.gasLimit,
-  );
+  const provider = new FakeProvider();
+  const web3 = new Web3();
+  web3.setProvider(provider);
+  const eth = GetEthereumHelperInstance(web3);
 
   test("buildInitiateTransaction", () => {
     const tx = eth.buildInitiateTransaction({
@@ -39,5 +37,44 @@ describe("EthereumHelpers test", () => {
       "0x9ffDD2871e06Df76E7245b8Be91E2af409F37Eda".toLowerCase(),
     );
     expect(params.netAmount).toBe(45369500000000000 - 10000000000000000);
+  });
+
+  test("encodeSignature", () => {
+    const encoded = eth.encodeSignature(
+      "0xda026bc6ff1a6aaa0838b930a9385c3eed6330c9858b0f8d175ce135376082190163416128cb97a2aed4e8bb98bb2a2fc2b0b4dd69b8595fd7ca8b0388a1076c1c",
+    );
+    expect(encoded).toBe(
+      "da026bc6ff1a6aaa0838b930a9385c3eed6330c9858b0f8d175ce135376082190163416128cb97a2aed4e8bb98bb2a2fc2b0b4dd69b8595fd7ca8b0388a1076c",
+    );
+  });
+
+  test("encodePublicKey", () => {
+    const encoded = eth.encodePublicKey(
+      "0x0492c4d2894b93d64189d6e2fc660e9090622148f8088c4bd967879d32ed438f9178852b4d34a23021da02b07ff7acc1ffb29404fd268f1c8cea7f92bfc24fdc48",
+    );
+    expect(encoded).toBe(
+      "0492c4d2894b93d64189d6e2fc660e9090622148f8088c4bd967879d32ed438f9178852b4d34a23021da02b07ff7acc1ffb29404fd268f1c8cea7f92bfc24fdc48",
+    );
+  });
+
+  test("estimateInitiateFees", async () => {
+    provider.injectResult(121);
+    provider.injectResult(12200);
+    const fees = await eth.estimateInitiateFees(
+      "0x0000000000000000000000000000000000000000",
+    );
+    expect(fees).toBe(121 * 12200);
+  });
+
+  test("estimateRedeemFees", async () => {
+    provider.injectResult(121);
+    const fees = await eth.estimateRedeemFees(
+      "0x0000000000000000000000000000000000000000",
+    );
+    const expectedMinerFees = 121 * config.contracts.ETH.testnet.gasLimit;
+    expect(fees).toStrictEqual({
+      minerFee: expectedMinerFees,
+      rewardForRedeem: 2 * expectedMinerFees,
+    });
   });
 });
