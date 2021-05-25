7c4c678324ed719bfd5033376ed6e612ca58cbd2
m-kus <mz@baking-bad.org>
2021-05-26 00:49:36 +0300

    Fix toolkit arg

--- test/tzip7.spec.ts
+++ test/tzip7.spec.ts
@@ -2,9 +2,10 @@ import { OperationContentsAndResultTransaction } from "@taquito/rpc";
 import { TezosToolkit } from "@taquito/taquito";
 import { GenerateMockRPCClient, GetTzBTCHelperInstance } from "./helpers/tezos";
 import initiate_tx from "./data/tzip7_initiate_tx.json";
+import config from "../src/config.json";
 
 describe("Tzip7Helpers test", () => {
-    const toolKit = new TezosToolkit();
+    const toolKit = new TezosToolkit(config.blockchains.tezos.rpc.testnet.rpc);
     toolKit["_context"].rpc = GenerateMockRPCClient("applied", 10000, 100, null);
     const tzbtc = GetTzBTCHelperInstance(toolKit);
 
