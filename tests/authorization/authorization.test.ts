0c429605115a340a00adbd8f543000590a6a046e
Andrew Skubarenko <andrew.skubarenko@gmail.com>
2022-08-01 22:53:44 +0300

    Replace the stopAuthTokenExpirationTracking method to dispose in the auth manager

--- tests/authorization/authorization.test.ts
+++ tests/authorization/authorization.test.ts
@@ -29,7 +29,7 @@ describe('Atomex authorization', () => {
   });
 
   afterEach(() => {
-    authorizationManager.stopAuthTokenExpirationTracking();
+    authorizationManager.dispose();
   });
 
   test('first authorization', async () => {
