52c29cd1cdad5c98694d49eef5c7b509973aedff
Maxim Kucherov <maxima-net@bk.ru>
2022-08-24 17:55:48 +0300

    Implement in-memory cache

---
+++ src/core/cache/cache.ts
@@ -0,0 +1,12 @@
+export type SetCacheOptions = {
+  absoluteExpirationMs: number;
+  slidingExpirationMs?: never;
+} | {
+  absoluteExpirationMs?: never;
+  slidingExpirationMs: number;
+};
+
+export interface Cache<K = string> {
+  get<T = unknown>(key: K): T | undefined;
+  set(key: K, value: unknown, options?: SetCacheOptions): void;
+}
---
+++ src/core/cache/inMemoryCache.ts
@@ -0,0 +1,51 @@
+import type { Cache, SetCacheOptions } from './cache';
+
+interface CacheEntry {
+  value: unknown;
+  watcherId: ReturnType<typeof setTimeout>;
+  options: SetCacheOptions;
+}
+
+export class InMemoryCache<K = string> implements Cache<K> {
+  private static readonly defaultCacheOptions: SetCacheOptions = {
+    absoluteExpirationMs: 1000 * 60 * 30,
+  };
+
+  private readonly cacheMap: Map<K, CacheEntry> = new Map();
+
+  constructor(
+    private readonly defaultCacheOptions: SetCacheOptions = InMemoryCache.defaultCacheOptions
+  ) { }
+
+  get<T = unknown>(key: K): T | undefined {
+    const entry = this.cacheMap.get(key);
+    if (!entry)
+      return undefined;
+
+    const isSlidingExpiration = this.getTimeoutAndIsSlidingExpiration(entry.options)[1];
+    if (isSlidingExpiration)
+      this.set(key, entry.value, entry.options);
+
+    return entry.value as T | undefined;
+  }
+
+  set(key: K, value: unknown, options = this.defaultCacheOptions): void {
+    const oldEntry = this.cacheMap.get(key);
+    if (oldEntry)
+      clearTimeout(oldEntry.watcherId);
+
+    const [timeout] = this.getTimeoutAndIsSlidingExpiration(options);
+    const watcherId = setTimeout(() => {
+      this.cacheMap.delete(key);
+    }, timeout);
+
+    const entry: CacheEntry = { value, watcherId, options };
+    this.cacheMap.set(key, entry);
+  }
+
+  private getTimeoutAndIsSlidingExpiration(options: SetCacheOptions): [timeout: number, isSlidingExpiration: boolean] {
+    return options.slidingExpirationMs !== undefined
+      ? [options.slidingExpirationMs, true]
+      : [options.absoluteExpirationMs, false];
+  }
+}
