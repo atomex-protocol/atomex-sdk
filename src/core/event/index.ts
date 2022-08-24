bf238a2a8697eadc7ad4358a2bf404a683318c01
Maxim Kucherov <maxima-net@bk.ru>
2022-08-24 16:17:22 +0300

    Refactor Core folder

---
+++ src/core/event/index.ts
@@ -0,0 +1,5 @@
+export { EventEmitter } from './eventEmitter';
+export type { PublicEventEmitter, ToEventEmitter, ToEventEmitters } from './eventEmitter';
+export { DeferredEventEmitter } from './deferredEventEmitter';
+export type { ToDeferredEventEmitter, ToDeferredEventEmitters } from './deferredEventEmitter';
+
