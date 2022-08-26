ee4c648dd9380e03d64487a95007e7c44f12a845
Maxim Kucherov <maxima-net@bk.ru>
2022-08-26 15:55:53 +0300

    Fix tests

--- tests/atomex/atomexProtocolV1.test.ts
+++ tests/atomex/atomexProtocolV1.test.ts
@@ -1,12 +1,12 @@
 import BigNumber from 'bignumber.js';
 
-import { getRedeemRewardInNativeToken, getRedeemRewardInToken, } from '../../src/blockchain/atomexProtocolV1/index';
+import { atomexProtocolV1Utils } from '../../src/blockchain/atomexProtocolV1/index';
 
-describe('Atomex | Atomex Protocol V1', () => {
+describe('Atomex Protocol V1 utils', () => {
   test('return correct redeem reward for native token', () => {
     const nativeTokenPriceInUsd = new BigNumber(2.15);
     const redeemFee = new BigNumber(0.011);
-    const redeemReward = getRedeemRewardInNativeToken(nativeTokenPriceInUsd, redeemFee);
+    const redeemReward = atomexProtocolV1Utils.getRedeemRewardInNativeToken(nativeTokenPriceInUsd, redeemFee);
     expect(redeemReward.estimated).toEqual(new BigNumber('0.021979139924875144'));
   });
 
@@ -14,7 +14,7 @@ describe('Atomex | Atomex Protocol V1', () => {
     const nativeTokenPriceInUsd = new BigNumber(2.15);
     const nativeTokenPriceInCurrency = new BigNumber(0.00006994);
     const redeemFee = new BigNumber(0.011);
-    const redeemReward = getRedeemRewardInToken(nativeTokenPriceInUsd, nativeTokenPriceInCurrency, redeemFee);
+    const redeemReward = atomexProtocolV1Utils.getRedeemRewardInToken(nativeTokenPriceInUsd, nativeTokenPriceInCurrency, redeemFee);
     expect(redeemReward.estimated).toEqual(new BigNumber('0.00000153722104634576757136'));
   });
 });
